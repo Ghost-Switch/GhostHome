51fda1202b570cd79674de42439b841d62abce16
Victor Isokpehi <victor.isokpehi@babbangona.com>
2024-02-06 21:39:50 +0100

    worked on the homepage

--- app/src/main/java/com/example/ghosthome/GhostHomeActivity.kt
+++ app/src/main/java/com/example/ghosthome/GhostHomeActivity.kt
@@ -2,53 +2,13 @@ package com.example.ghosthome
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.view.animation.Animation
-import android.view.animation.AnimationUtils
-import android.widget.Adapter
-import android.widget.Toast
-import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
-import androidx.fragment.app.Fragment
-import androidx.viewpager2.widget.ViewPager2
-import com.example.ghosthome.databinding.ActivityGhostHomeBinding
-import com.example.ghosthome.databinding.FragmentAddRoomBinding
-import com.example.ghosthome.ghostHome.OuterTabAdapter
-import com.example.ghosthome.ghostHome.livingRoom.KitchenFragment
-import com.example.ghosthome.ghostHome.livingRoom.LivingRoomFragment
-import com.google.android.material.tabs.TabLayout
-import com.google.android.material.tabs.TabLayoutMediator
 
-class GhostHomeActivity : Fragment() {
+class GhostHomeActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityGhostHomeBinding
     private lateinit var drawerLayout: DrawerLayout
-    private lateinit var tabLayout: TabLayout
-    private lateinit var viewPager : ViewPager2
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        binding = ActivityGhostHomeBinding.inflate(layoutInflater)
-        setBinding()
-        return  binding.root
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ghost_home)
     }
-
-    private fun setBinding() {
-        val homeTabLayout = binding.homeTab
-        tabLayout = homeTabLayout.backgroundTabLayout
-        viewPager = binding.viewPager
-        val adapter = OuterTabAdapter(parentFragmentManager, lifecycle)
-        adapter.addFragment(LivingRoomFragment(), getString(R.string.living_room))
-        adapter.addFragment(KitchenFragment(), getString(R.string.kitchen))
-
-        viewPager.adapter = adapter
-        TabLayoutMediator(tabLayout, viewPager){tab, position ->
-            tab.text = adapter.getTitle(position)
-        }.attach()
-    }
-
 }
--- app/src/main/java/com/example/ghosthome/login/LogInFragment.java
+++ app/src/main/java/com/example/ghosthome/login/LogInFragment.java
@@ -3,64 +3,49 @@ package com.example.ghosthome.login;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.ghosthome.R;
+import com.example.ghosthome.databinding.FragmentLogInBinding;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link LogInFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class LogInFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private FragmentLogInBinding binding;
+    private NavController navController;
 
     public LogInFragment() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment LogInFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static LogInFragment newInstance(String param1, String param2) {
-        LogInFragment fragment = new LogInFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_log_in, container, false);
+        binding = FragmentLogInBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // Initialize NavController
+        navController = Navigation.findNavController(view);
+        setBinding();
+    }
+    private void setBinding() {
+        binding.btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Navigate to the appropriate destination
+                navController.navigate(R.id.action_logInFragment_to_homeNameFragment);
+            }
+        });
     }
 }
